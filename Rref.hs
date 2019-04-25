a254eb5efc9ead202e967c647d35f0a825139e8a
Erik Brink <Erkky99@gmail.com>
2019-04-25 13:08:03 +0200

    flipRows now works

--- Rref.hs
+++ Rref.hs
@@ -8,15 +8,9 @@ exMatrix = [[1,2,3],
 checkElem :: [[Int]] -> Int -> Int -> Int
 checkElem m r c = (m !! r) !! c
 
-flipRows :: [[Int]] -> Int -> Int -> [[Int]] {-Doesn't work yet-}
-flipRows m row1 row2 = take row1 m ++ [(m !! row2)] ++ {-inbetween-} ++
+flipRows :: [[Int]] -> Int -> Int -> [[Int]] {-row1 has to be less than row2-}
+flipRows m row1 row2 = take row1 m ++ [(m !! row2)] ++ (take (row2-(row1+1)) (drop (row1 + 1) m)) ++
                        [(m !! row1)] ++ (drop (row2 + 1) m)
-
-                    {- take row1 m ++ [(m !! row2)] ++ take (row1 + 1) (drop (row1 + 1) m) ++
-                       [(m !! row1)] ++ take (row2 + 1) (drop (row2 + 1) m)
-                       [ x | x <- m, (x == (m !! row1))] ++
-                       [ x | x <- m, (x == (m !! row2))] ++
-                       [ x | x <- m, (x /= (m !! row2)) || (x /= (m !! row1))] -}
 
 mulRow :: [[Int]] -> Int -> Int -> [[Int]]
 mulRow m i k = take (i) m ++ [(map (*k) (m !! i))] ++ drop (i+1) m
@@ -25,3 +19,10 @@ mulAddRow :: [[Int]] -> Int -> Int -> Int -> [[Int]]
 mulAddRow m i1 i2 k = take (i2) m ++
                       [(zipWith (+) ((mulRow m i1 k) !! i1) (m !! i2))] ++ {-i2-}
                       drop (i2+1) m
+
+
+
+{-flipRowsLite :: [Int] -> Int -> Int -> [Int]
+flipRowsLite l i1 i2 = take i1 l ++ [(l !! i2)] ++ (take (i2-(i1+1)) (drop (i1 + 1) l)) ++
+                      [(l !! i1)] ++ (drop (i2 + 1) l)
+-}
